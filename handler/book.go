9235e28f6a9e39b761b56153dbeb75ae5ec5fdb2
diosurandito <diosurandito@gmail.com>
2022-02-17 09:40:46 +0700

    add endpoint get all books

--- handler/book.go
+++ handler/book.go
@@ -10,39 +10,46 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-func RootHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"name": "Dio",
-		"bio":  "Programmer",
-	})
+type bookHandler struct {
+	bookService book.Service
 }
 
-func BooksHandler(c *gin.Context) {
-	id := c.Param("id")
-	title := c.Param("title")
+func NewBookHandler(bookService book.Service) *bookHandler {
+	return &bookHandler{bookService}
+}
+
+func (h *bookHandler) GetBooks(c *gin.Context) {
+	books, err := h.bookService.FindAll()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+		return
+	}
+
+	var booksResponse []book.BookResponse
+	for _, b := range books {
+		bookResponse := book.BookResponse{
+			ID:          b.ID,
+			Title:       b.Title,
+			Price:       b.Price,
+			Description: b.Description,
+			Rating:      b.Rating,
+		}
+
+		booksResponse = append(booksResponse, bookResponse)
+	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"id":     id,
-		"title":  title,
-		"writer": "Dio",
+		"data": booksResponse,
 	})
 
 }
 
-func QueryHandler(c *gin.Context) {
-	title := c.Query("title")
-	price := c.Query("price")
-	c.JSON(http.StatusOK, gin.H{
-		"title":  title,
-		"price":  price,
-		"writer": "Dio",
-	})
-}
+func (h *bookHandler) PostBooksHandler(c *gin.Context) {
+	var bookRequest book.BookRequest
 
-func PostBooksHandler(c *gin.Context) {
-	var bookInput book.BookInput
-
-	err := c.ShouldBindJSON(&bookInput)
+	err := c.ShouldBindJSON(&bookRequest)
 	if err != nil {
 		errorMessages := []string{}
 		for _, e := range err.(validator.ValidationErrors) {
@@ -56,9 +63,15 @@ func PostBooksHandler(c *gin.Context) {
 		return
 	}
 
+	book, err := h.bookService.Create(bookRequest)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"title":     bookInput.Title,
-		"price":     bookInput.Price,
-		"sub_title": bookInput.SubTitle,
+		"data": book,
 	})
 }
--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"log"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka_api_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("DB connection error")
+	}
+	db.AutoMigrate(&book.Book{})
+
+	bookRepository := book.NewRepository(db)
+	bookService := book.NewService(bookRepository)
+	bookHandler := handler.NewBookHandler(bookService)
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
-	v1.GET("/", handler.RootHandler)
-	v1.GET("/books/:id/:title", handler.BooksHandler)
-	v1.POST("/books", handler.PostBooksHandler)
-	v1.GET("/query", handler.QueryHandler)
+	v1.POST("/books", bookHandler.PostBooksHandler)
+	v1.GET("/books", bookHandler.GetBooks)
 
 	router.Run(":8888")
 
